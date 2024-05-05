bef4355ac4ff8d7f9772edd76f1a06ee3fc34ba2
Luis Alejandro Muñoz Garcia <sena.empresa2012@gmail.com>
2024-05-04 19:48:16 -0500

    estructura del aprendiz

---
+++ src/api/aprendiz/routes/aprendiz.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * aprendiz router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::aprendiz.aprendiz');
---
+++ src/api/aprendiz/services/aprendiz.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * aprendiz service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::aprendiz.aprendiz');
